cdb584342e2e05b30bda3246d17ba0a95a068fcb
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-26 18:24:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "element"
+  add_element_to_end_of_array.push "element"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop "element"
 end
 
 def remove_element_from_start_of_array(array)
